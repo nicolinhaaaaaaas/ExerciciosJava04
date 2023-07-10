431ebece285aff9d9e2902f3f213c079725b16be
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-10 09:27:37 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+package questao38;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+    //INCOMPLETO, nao consegui fazer
+		
+		EntradaEmAgenda entrada1 = new EntradaEmAgenda("08:38", 10, 07, 2023, "Hoje eu fiz essa atividade");
+		EntradaEmAgenda entrada2 = new EntradaEmAgenda("17:30", 12, 07, 2023, "Dei o remedio do meu cachorro agora");
+		EntradaEmAgenda entrada3 = new EntradaEmAgenda("05:23", 18, 07, 2023, "Eh meu aniversario e nao sei o que fazer");
+		EntradaEmAgenda entrada4 = new EntradaEmAgenda("12:30", 25, 07, 2023, "Eh o aniversario do meu irmao");
+		EntradaEmAgenda entrada5 = new EntradaEmAgenda("23:11", 18, 07, 2023, "Chorei ate dormir.");
+		
+		
+		System.out.println(entrada1);
+		
+		}
+		
+
+	}
+
+}
