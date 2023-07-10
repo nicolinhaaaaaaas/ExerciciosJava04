ce3685fe90930a809524ed8a7292e70663e923a2
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-10 09:28:21 -0300

    Create Agenda.java

---
+++ Agenda.java
@@ -0,0 +1,62 @@
+package questao38;
+import java.util.*;
+public class Agenda implements EntradaEmAgenda{
+
+	Scanner scanner = new Scanner(System.in);
+	
+	ArrayList <String>
+	
+	EntradaEmAgenda entrada = new EntradaEmAgenda(String hora, int dia, int mes, int ano, String assunto);
+	
+	public void setHora() {
+		
+	}
+	
+	public void setDia() {
+		
+	}
+	
+	public void setMes() {
+		
+	}
+	
+	public void setAno() {
+		
+	}
+	
+	public void setAssunto() {
+		
+	}
+	
+	
+	
+	public String getHora() {
+		return hora;
+	}
+	
+	public int getDia() {
+		return dia;
+	}
+	
+	public int getMes() {
+		return mes;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public String getAssunto() {
+		return assunto;
+	}
+	
+	
+	
+	public void adicionar() {
+		
+	}
+	
+	public void listaDia() {
+		
+	}
+}
